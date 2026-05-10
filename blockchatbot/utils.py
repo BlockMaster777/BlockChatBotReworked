48550c90313199ceacb265ca6e6cca9f4b2d1819
BlockMaster <145277345+BlockMaster777@users.noreply.github.com>
2026-05-10 19:54:11 +0300

    Add util for db config

---
+++ blockchatbot/utils.py
@@ -0,0 +1,32 @@
+#  BlockChatBotReworked
+#  Copyright (C) 2026  BlockMaster
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# coding=utf8
+
+from configparser import ConfigParser
+
+
+def get_config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(f'Section {section} not found in the {filename}')
+    return db
